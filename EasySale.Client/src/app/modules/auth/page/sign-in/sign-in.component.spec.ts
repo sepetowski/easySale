229275202b3467efa6ee249056145bcd42630162
bushmeen <bushmeen@gmail.com>
2024-05-17 17:05:14 +0200

    add login page with tests

--- EasySale.Client/src/app/modules/auth/page/sign-in/sign-in.component.spec.ts
+++ EasySale.Client/src/app/modules/auth/page/sign-in/sign-in.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInComponent } from './sign-in.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../../../app.routes';
+import {
+  MAX_USERNAME_LENGTH,
+  MIN_USERNAME_LENGTH,
+} from '../../../../shared/validators/contstants';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
@@ -8,10 +14,10 @@ describe('SignInComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SignInComponent]
-    })
-    .compileComponents();
-    
+      imports: [SignInComponent],
+      providers: [provideRouter(routes)],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +25,56 @@ describe('SignInComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should require valid username (too short username)', () => {
+    component.signInForm.setValue({
+      username: '_',
+      password: 'GoodPassword1',
+    });
+
+    expect(component.signInForm.controls.username.errors).toEqual({
+      minlength: { requiredLength: MIN_USERNAME_LENGTH, actualLength: 1 },
+    });
+  });
+
+  it('should require valid username (too long username)', () => {
+    component.signInForm.setValue({
+      username: '1234567890123456789012345678901', //31
+      password: 'GoodPassword1',
+    });
+
+    expect(component.signInForm.controls.username.errors).toEqual({
+      maxlength: { requiredLength: MAX_USERNAME_LENGTH, actualLength: 31 },
+    });
+  });
+
+  it('should require valid password', () => {
+    component.signInForm.setValue({
+      username: 'User',
+      password: 'wrongPassword',
+    });
+
+    expect(component.signInForm.controls.password.errors).toEqual({
+      notValidPassword: true,
+    });
+  });
+
+  it('should require all fileds', () => {
+    component.signInForm.setValue({
+      username: '',
+      password: '',
+    });
+
+    expect(component.signInForm.valid).toEqual(false);
+  });
+
+  it('should form be valid for all valid fields', () => {
+    component.signInForm.setValue({
+      username: 'User',
+      password: 'GoodPassword1',
+    });
+
+    expect(component.signInForm.valid).toEqual(true);
   });
 });
--- EasySale.Client/src/app/modules/auth/page/sign-in/sign-in.component.ts
+++ EasySale.Client/src/app/modules/auth/page/sign-in/sign-in.component.ts
@@ -1,12 +1,69 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { InputErrorMessageComponent } from '../../../../shared/components/input-error-message/input-error-message.component';
+import {
+  MIN_USERNAME_LENGTH,
+  MAX_USERNAME_LENGTH,
+} from '../../../../shared/validators/contstants';
+import { validatePassword } from '../../../../shared/validators/validate-password.validator';
+
+interface SignInForm {
+  username: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    FloatLabelModule,
+    PasswordModule,
+    DividerModule,
+    ReactiveFormsModule,
+    InputErrorMessageComponent,
+    RouterLink,
+    IconFieldModule,
+    InputIconModule,
+  ],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  signInForm = new FormGroup<SignInForm>({
+    username: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(MIN_USERNAME_LENGTH),
+        Validators.maxLength(MAX_USERNAME_LENGTH),
+      ],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, validatePassword],
+      nonNullable: true,
+    }),
+  });
 
+  onSubmit() {
+    console.log(this.signInForm);
+    this.signInForm.reset();
+  }
 }
